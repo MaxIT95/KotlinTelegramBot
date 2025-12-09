b2e5dbe292df865bb0d66ad27376bc91b4bd0498
kuleshow <kuleshov995@yandex.ru>
2025-12-09 23:30:04 +0300

    KTB-04: решение

--- src/main/java/org/example/Main.kt
+++ src/main/java/org/example/Main.kt
@@ -5,9 +5,17 @@ import java.io.File
 fun main() {
     val file = File("src/main/resources/words.txt")
 
+    var dictionaries = mutableListOf<Word>()
     val lines = file.readLines()
 
     lines.forEach {
-        println(it)
+        val line = it.split("|")
+        val word = Word(line[0], line[1])
+
+        if (line[2].isNotEmpty()) {
+            word.correctAnswersCount = line[2].toInt()
+        }
+        dictionaries.add(word)
     }
+    println(dictionaries)
 }
